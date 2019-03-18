e1cadc10a1c503096e7f1b38345071d257604ae8
Ryan <ryan@dksit.com>
2019-03-18 16:11:29 -0600

    completed project

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,53 +1,33 @@
-let messages= [];
+let messages = [];
 let id = 0;
+
 
 module.exports = {
     create: (req, res) => {
-    const {text, time} = req.body;
-    messages.push({id, text, time});
-    id++;
-    res.status(200).send(messages);
+        const {text, time} = req.body
+        messages.push({id, text, time})
+        id++
+        res.status(200).send(messages)
     },
-
     read: (req, res) => {
-    
-    res.status(200).send(messages);
-
+        res.status(200).send(messages)
     },
-
     update: (req, res) => {
-    // update the text property of a message using the text value from the request body.
-    const {text} = req.body;
-    
-    //determine which message to update based on the value of id from the request url parameters
-    const updateID = req.params.id;
-    
-    //use .findIndex to get the index where the ids match.
-    const messageIndex = messages.findIndex(message => message.id == updateID);
-    let message = messages[messageIndex];
-
-    messages [messageIndex] = {
-        id: message.id,
-        text: text || message.text,
-        time: message.time
-    };
-    //return the updated messages array.
-    res.status(200).send (messages);
+        const {text} = req.body
+        const updateId = req.params.id
+        const messageIndex = messages.findIndex(e => e.id == updateId)
+        let message = messages[messageIndex]
+        messages[messageIndex] = {
+            id: message.id,
+            text: text || message.text,
+            time: message.time
+        }
+        res.status(200).send(messages)
     },
-
     delete: (req, res) => {
-    //delete a message using the value of id from the request url parameters.
-    const deleteID = req.params.id;
-
-    //.findIndex again with the id to get the index of the message object
-    messageIndex = messages.findIndex(message => message.id == deleteID);
-
-    //use .splice to remove it from the messages array. 
-    messages.splice(messageIndex, 1);
-
-    //send the updated messages array.
-    res.status(200).send(messages);
+        const deleteId = req.params.id
+        messageIndex = messages.findIndex(e => e.id == deleteId)
+        messages.splice(messageIndex, 1)
+        res.status(200).send(messages)
     }
-    
-
 }
--- server/index.js
+++ server/index.js
@@ -1,28 +1,15 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-
+const express = require('express')
 const app = express();
-const mc = require('./controllers/messages_controller');
- 
-app.use(bodyParser.json() );
-app.use(express.static(__dirname + '/../public/build'));
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+const mc = require('../server/controllers/messages_controller')
+
+const url = "/api/messages"
+
+app.get(url, mc.read)
+app.post(url, mc.create)
+app.put(`${url}/:id`, mc.update)
+app.delete(`${url}/:id`, mc.delete)
+
 const port = 3001;
-app.listen(port, () => {console.log(`SERVER is running..on ${port}`); } );
-
-//make a messagesBaseUrl variable so that if the URL ever changes we won't have to update in four different places.
-//The messagesBaseUrl should equal /api/messages.
-const messageBaseUrl = "/api/messages";
-
-//create endpoints
-
-//use the built-in methods express gives us to create endpoints. We'll use post for create
-app.post(messageBaseUrl, mc.create);
-
-// use get for read
-app.get(messageBaseUrl, mc.read);
-
-// use put for update and add on a url parameter of id
-app.put(`${messageBaseUrl}:id`, mc.update);
-
-// use delete for delete and add on a url parameter of id
-app.delete(`${messageBaseUrl}:id`, mc.delete);
+app.listen(port,() => {console.log(`Running on ${port}`)})
